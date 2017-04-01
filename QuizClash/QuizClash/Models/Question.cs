31c02ec0a69cfeadfaefbb0da4f562dcb9b5659c
Mikołaj Waśniewski <wasniewskimikolaj@gmail.com>
2017-04-01 18:06:48 +0200

    Działa baza

---
+++ QuizClash/QuizClash/Models/Question.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace QuizClash.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string AnswerA { get; set; }
+        public string AnswerB { get; set; }
+        public string AnswerC { get; set; }
+        public string AnswerD { get; set; }
+        public int CorrectAnswer { get; set; }
+    }
+
+    public class QuestionDBContext : DbContext
+    {
+        public DbSet<Question> Questions { get; set; }
+    }
+}
